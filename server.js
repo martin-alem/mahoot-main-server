f7d8a85a5016ecbb46e0d85b0416fd79b96b813d
Martin Alemajoh <alemajohmartin@gmail.com>
2021-10-21 20:00:30 -0400

    setup project

---
+++ server.js
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+//connect to database
+
+const app = express();
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: 200, statusText: "OK", message: "server up and running" });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ status: 404, statusText: "fail", message: "The path you are requesting does not exist" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("Main server listening on port " + PORT);
+});
